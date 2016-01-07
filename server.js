1b818f8bc8a8d94afcb0e098cc3d092cb8715e9e
an2netto <an2netto@gmail.com>
2016-01-08 00:13:12 +0200

    reorganisation2

--- server.js
+++ server.js
@@ -1,50 +1,23 @@
-// HTTP Portion
-var http = require('http');
+// load express
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
 // Path module
-var path = require('path');
-
+// var path = require('path');
 // Using the filesystem module
-var fs = require('fs');
+// var fs = require('fs');
 
-var server = http.createServer(handleRequest);
-server.listen(8080);
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-console.log('Server started on port 8080');
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+server.listen(port);
 
-function handleRequest(req, res) {
-  // What did we request?
-  var pathname = req.url;
-  
-  // If blank let's ask for index.html
-  if (pathname == '/') {
-    pathname = '/index.html';
-  }
-  
-  // Ok what's our file extension
-  var ext = path.extname(pathname);
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/public/index.html');
+});
 
-  // Map extension to file type
-  var typeExt = {
-    '.html': 'text/html',
-    '.js':   'text/javascript',
-    '.css':  'text/css'
-  };
-  // What is it?  Default to plain text
-
-  var contentType = typeExt[ext] || 'text/plain';
-
-  // User file system module
-  fs.readFile(__dirname + pathname,
-    // Callback function for reading
-    function (err, data) {
-      // if there is an error
-      if (err) {
-        res.writeHead(500);
-        return res.end('Error loading ' + pathname);
-      }
-      // Otherwise, send the data, the contents of the file
-      res.writeHead(200,{ 'Content-Type': contentType });
-      res.end(data);
-    }
-  );
-}
+console.log('node running on port ' + port);
